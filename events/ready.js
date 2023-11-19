a377edd1e249462645e7f8340cf3ccd3a86e6c3a
christopher simmons <kadensdad93@gmail.com>
2023-11-19 00:57:21 -0800

    removed ai code

--- events/ready.js
+++ events/ready.js
@@ -36,59 +36,58 @@ module.exports = {
                     console.log("Successfully registered commands locally.");
                 }
 
-                async function getFunFact() {
-                    try {
-                        response = prompts.funFactPrompt();
-                        console.log(response.anime)
-                        const completion = await ai.chat.completions.create({
-                            messages: [{ role: 'user', content: response.prompt }],
-                            model: 'gpt-4-1106-preview',
-                        });
-                        console.log(completion.choices[0].message.content)
-                        return {
-                            fact: completion.choices[0].message.content,
-                            anime: response.anime
-                        }
-                    } catch (error) {
-                        console.error(error);
-                        throw new Error("There was an issue with getting the fun fact. Please seek developer support.");
-                    }
-                }
+                // async function getFunFact() {
+                //     try {
+                //         response = prompts.funFactPrompt();
+                //         console.log(response.anime)
+                //         const completion = await ai.chat.completions.create({
+                //             messages: [{ role: 'user', content: response.prompt }],
+                //             model: 'gpt-4-1106-preview',
+                //         });
+                //         console.log(completion.choices[0].message.content)
+                //         return {
+                //             fact: completion.choices[0].message.content,
+                //             anime: response.anime
+                //         }
+                //     } catch (error) {
+                //         console.error(error);
+                //         throw new Error("There was an issue with getting the fun fact. Please seek developer support.");
+                //     }
+                // }
 
 
-                async function reviewPromptTake() {
-                    try {
-                        let prompt;
+                // async function reviewPromptTake() {
+                //     try {
+                //         let prompt;
                 
-                        prompt = prompts.reviewPrompt();
-                        const completion = await ai.chat.completions.create({
-                            messages: [{ role: 'user', content: prompt }],
-                            model: 'gpt-4-1106-preview',
-                        });
-                        console.log(completion.choices[0].message.content)
-                        return completion.choices[0].message.content;
-                    } catch (error) {
-                        console.error(error);
-                        throw new Error("There was an issue with getting the question. Please seek developer support.");
-                    }
-                }
+                //         prompt = prompts.reviewPrompt();
+                //         const completion = await ai.chat.completions.create({
+                //             messages: [{ role: 'user', content: prompt }],
+                //             model: 'gpt-4-1106-preview',
+                //         });
+                //         console.log(completion.choices[0].message.content)
+                //         return completion.choices[0].message.content;
+                //     } catch (error) {
+                //         console.error(error);
+                //         throw new Error("There was an issue with getting the question. Please seek developer support.");
+                //     }
+                // }
 
 
-                // Set an interval to run every hour (3600000 milliseconds)
-                setInterval(async () => {
-                    // Loop through all guilds (servers) the bot is a member of
-                    const channel = client.channels.cache.get(process.env.PROD_CHANNEL_ID_FOR_HOTTAKES)
-                    const response = await getFunFact()
+                // // Set an interval to run every hour (3600000 milliseconds)
+                // setInterval(async () => {
+                //     // Loop through all guilds (servers) the bot is a member of
+                //     const channel = client.channels.cache.get(process.env.PROD_CHANNEL_ID_FOR_HOTTAKES)
+                //     const response = await getFunFact()
 
-                    // console.log(response.fact)
+                //     // console.log(response.fact)
                 
-                    const embedVar = new EmbedBuilder()
-                        .setTitle(`💡 ${response.anime} - Fun Fact`)
-                        .setDescription(response.fact)
-                        .setTimestamp()
-                    channel.send({ embeds: [embedVar] });
-                }, 7200000 ); // 54000000
-
+                //     const embedVar = new EmbedBuilder()
+                //         .setTitle(`💡 ${response.anime} - Fun Fact`)
+                //         .setDescription(response.fact)
+                //         .setTimestamp()
+                //     channel.send({ embeds: [embedVar] });
+                // }, 7200000 ); // 54000000
 
 
             } catch (err) {
